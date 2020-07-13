3c323f62f0eaee2181937384b83cc466952820cc
paul <jiandongquan@126.com>
2020-07-13 23:22:05 +0800

    增加中文注释

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -7,24 +7,45 @@
  * fixedHeader - 固定 Header : boolean
  * fixSiderbar - 固定左侧菜单栏 ： boolean
  * contentWidth - 内容区布局： 流式 |  固定
- *
  * storageOptions: {} - Vue-ls 插件配置项 (localStorage/sessionStorage)
- *
  */
 
 export default {
-  navTheme: 'dark', // theme for nav menu
-  primaryColor: '#52C41A', // primary color of ant design
-  layout: 'sidemenu', // nav menu position: `sidemenu` or `topmenu`
-  contentWidth: 'Fluid', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
-  fixedHeader: false, // sticky header
-  fixSiderbar: false, // sticky siderbar
+  // 侧边栏的主题，包括两种：dark、light
+  navTheme: 'light',
+
+  // Ant Design组件的默认主题颜色，如果修改颜色不生效，则需要清理localstorage
+  primaryColor: '#92C41A',
+
+  // 整体布局-菜单栏的位置，包括两种：sidemenu、topmenu
+  layout: 'sidemenu',
+
+  // 内容区域布局，只有菜单栏位置为topmenu时有效，包括两种：Fluid-流式布局、Fixed-固定布局
+  contentWidth: 'Fluid',
+
+  // 固定Header
+  fixedHeader: false,
+
+  // 固定侧边栏，只有菜单栏位置为sidemenu时有效；
+  fixSiderbar: false,
+
+  // 色弱模式
   colorWeak: false,
+
+  // 菜单--本地？
   menu: {
     locale: true
   },
+
+  // 应用标题
   title: 'Ant Design Pro',
+
+  // pwa模式
   pwa: false,
+
+  // 图标库URL
   iconfontUrl: '',
+
+  // 是否生产版本；
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true'
 }
